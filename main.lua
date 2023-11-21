69adb4ed4130fba05b02dd009743b17c2811ef3c
Saint <87243692+Saint0-0@users.noreply.github.com>
2023-11-21 16:57:56 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,19 +1,60 @@
 function InitialiseFarm()
-    local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+
+	-- Populating the shared.SkillsTable
+	shared.SkillsTable = HttpService:JSONDecode(readfile("Exulus/Cache/StagesCache.exu"))
+
     local Window = Fluent:CreateWindow({
         Title = "Ξxulus",
         SubTitle = "by saint.dev",
         TabWidth = 160,
-        Size = UDim2.fromOffset(580, 460),
+        Size = UDim2.fromOffset(200, 200),
         Acrylic = true,
         Theme = "Dark"
     })
 
+	-- Tabs
     local Farm = Window:AddTab({Title = "Farm", Icon = "Car"})
     local Config = Window:AddTab({Title = "Farm configuration", Icon = "Car"})
     local Modules = Window:AddTab({Title = "Weapon modules", Icon = "Car"})
 
-    
-end
+	-- Functions
+	local function SaveData(Data)
+		local TemporaryVariable = HttpService:JSONEncode(Data)
+		writefile("Exulus/Cache/StagesCache.exu", TemporaryVariable)
+	end
 
-InitialiseFarm()
+    -- Farm config tab
+    Config:AddToggle("Skill C", {
+		Title = "Auto click",
+		Default = shared.SkillsTable.C,
+		Callback = function(Value)
+			shared.SkillsTable.C = Value
+			SaveData(shared.SkillsTable)
+		end
+	})
+	Config:AddToggle("Skill E", {
+		Title = "Auto click",
+		Default = shared.SkillsTable.E,
+		Callback = function(Value)
+			shared.SkillsTable.E = Value
+			SaveData(shared.SkillsTable)
+		end
+	})
+	Config:AddToggle("Skill F", {
+		Title = "Auto click",
+		Default = shared.SkillsTable.F,
+		Callback = function(Value)
+			shared.SkillsTable.F = Value
+			SaveData(shared.SkillsTable)
+		end
+	})
+	Config:AddToggle("Skill R", {
+		Title = "Auto click",
+		Default = shared.SkillsTable.R,
+		Callback = function(Value)
+			shared.SkillsTable.R = Value
+			SaveData(shared.SkillsTable)
+		end
+	})
+
+end
