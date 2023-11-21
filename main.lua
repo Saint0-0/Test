6eb1184f8ccfad3a2774cd1a64c9ca6babf2fa90
Saint <87243692+Saint0-0@users.noreply.github.com>
2023-11-21 18:31:08 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,40 +1,3 @@
-shared.SkillsTable = {
-	C = false,
-	E = false,
-	F = false,
-	R = false,
-	Stage1 = false,
-	Stage2 = false,
-	Stage3 = false,
-	Stage4 = false,
-	Stage5 = false,
-	Stage6 = false,
-	SelectedStage = "Stage1"
-}
-local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
-local HttpService = game:GetService("HttpService")
-
-if writefile then
-	if not isfolder("Exulus") then
-		makefolder("Exulus")
-	end
-	if not isfolder("Exulus/Cache") then
-		makefolder("Exulus/Cache")
-	end
-	if not isfile("Exulus/Cache/ServerHopData.json") then
-		writefile("Exulus/Cache/ServerHopData.json", "")
-	end
-	if not isfile("Exulus/Cache/Webhook.exu") then
-		writefile("Exulus/Cache/Webhook.exu", "Tm8gV2ViaG9vayBTdG9yZWQu")
-	end
-	if not isfile("Exulus/Cache/Failed_Webhook_Post_Resort.txt") then
-		writefile("Exulus/Cache/Failed_Webhook_Post_Resort.txt", "")
-	end
-	if not isfile("Exulus/Cache/StagesCache.exu") then
-		writefile("Exulus/Cache/StagesCache.exu", HttpService:JSONEncode(shared.SkillsTable))
-	end
-end
-
 function InitialiseFarm()
 
 	-- Populating the shared.SkillsTable
@@ -74,7 +37,7 @@ function InitialiseFarm()
 		Title = "Select the skills to use",
 		Content = ""
 	})
-    Config:AddToggle({
+    Config:AddToggle("SkillC", {
 		Title = "Skill C",
 		Default = shared.SkillsTable.C,
 		Callback = function(Value)
@@ -82,7 +45,7 @@ function InitialiseFarm()
 			SaveData(shared.SkillsTable)
 		end
 	})
-	Config:AddToggle({
+	Config:AddToggle("SkillE", {
 		Title = "Skill E",
 		Default = shared.SkillsTable.E,
 		Callback = function(Value)
@@ -90,7 +53,7 @@ function InitialiseFarm()
 			SaveData(shared.SkillsTable)
 		end
 	})
-	Config:AddToggle({
+	Config:AddToggle("SkillF", {
 		Title = "Skill F",
 		Default = shared.SkillsTable.F,
 		Callback = function(Value)
@@ -98,7 +61,7 @@ function InitialiseFarm()
 			SaveData(shared.SkillsTable)
 		end
 	})
-	Config:AddToggle({
+	Config:AddToggle("SkillR", {
 		Title = "Skill R",
 		Default = shared.SkillsTable.R,
 		Callback = function(Value)
@@ -144,5 +107,3 @@ function InitialiseFarm()
 	Window:SelectTab(1)
 
 end
-
-InitialiseFarm()
