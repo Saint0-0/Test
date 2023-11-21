7ef4582fc015deff52b50124cbdc0ef824d43bb7
Saint <87243692+Saint0-0@users.noreply.github.com>
2023-11-21 16:29:36 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,19 @@
-dsa
+function InitialiseFarm()
+    local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+    local Window = Fluent:CreateWindow({
+        Title = "Ξxulus",
+        SubTitle = "by saint.dev",
+        TabWidth = 160,
+        Size = UDim2.fromOffset(580, 460),
+        Acrylic = true,
+        Theme = "Dark"
+    })
+
+    local Farm = Window:AddTab({Title = "Farm", Icon = "Car"})
+    local Config = Window:AddTab({Title = "Farm configuration", Icon = "Car"})
+    local Modules = Window:AddTab({Title = "Weapon modules", Icon = "Car"})
+
+    
+end
+
+InitialiseFarm()
